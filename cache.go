426fc6283e1a9676dafa2ec30794e95ce90f5f73
Alexandr Primak <primak.aa@gmail.com>
2025-01-20 14:58:50 +0300

    Exported ErrKeyNotFound error.

--- cache.go
+++ cache.go
@@ -11,7 +11,7 @@ import (
 // defaultCheckInterval sets the default value of memory checks for expired entries.
 const defaultCheckInterval = 10 * time.Second
 
-var errKeyNotFound = errors.New("key is missing in the cache")
+var ErrKeyNotFound = errors.New("key is missing in the cache")
 
 type record struct {
 	value     any
@@ -77,7 +77,7 @@ func (m *MicroCache) processExpiration() {
 	}
 }
 
-// Get entry from the cache by "key" if present, otherwise it returns errKeyNotFound error.
+// Get entry from the cache by "key" if present, otherwise it returns ErrKeyNotFound error.
 func (m *MicroCache) Get(_ context.Context, key string) (any, error) {
 	m.mu.RLock()
 	defer m.mu.RUnlock()
@@ -86,7 +86,7 @@ func (m *MicroCache) Get(_ context.Context, key string) (any, error) {
 		return r.value, nil
 	}
 
-	return nil, errKeyNotFound
+	return nil, ErrKeyNotFound
 }
 
 // Set the entry to cache, "expiration" interval determines how long the entry will remain in the cache.
